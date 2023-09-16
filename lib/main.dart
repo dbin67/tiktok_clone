89edd0d262b9d440305c2a419bd8bb8e7255a870
donghyun <dbin67@naver.com>
2023-09-16 13:58:43 +0900

    #3 PROJECT SETUP

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: const Row(children: [
+            Text('hello'),
+            Gaps.h20,
+            Text('hello'),
+          ]),
+        ),
+      ),
+    );
+  }
+}
