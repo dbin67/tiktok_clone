4a283d3f864a0c51c6fa56ee0300283b85a67c62
dbin67 <dbin67@naver.com>
2023-09-18 16:50:29 +0900

    #5 ONBOARDING

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -12,21 +13,22 @@ class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'TikTok Clone',
-        theme: ThemeData(
-          primaryColor: const Color(0xFFE9435A),
-          appBarTheme: const AppBarTheme(
-            foregroundColor: Colors.black,
-            backgroundColor: Colors.white,
-            elevation: 0,
-            titleTextStyle: TextStyle(
-              fontSize: Sizes.size16 + Sizes.size2,
-              fontWeight: FontWeight.w600,
-              color: Colors.black,
-            ),
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+        appBarTheme: const AppBarTheme(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
           ),
-          scaffoldBackgroundColor: Colors.white,
         ),
-        home: const SignUpScreen());
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const InterestsScreen(),
+    );
   }
 }
